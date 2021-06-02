832dcf013359cbbb9ae43caab5c191dfe7585f76
vuva <vuanh.vu@ikt.uni-hannover.de>
2021-06-02 12:20:12 +0200

    isolate traefik and the services in a docker network

--- start-containers.sh
+++ start-containers.sh
@@ -1,17 +1,23 @@
-# This is not the best way to setup. To make it more secured, you should create a docker network and connect all containers to traefik only in that network.
 # Having evrything in a docker-compose would be cleaner, but QNAP Container station couldn't manage docer-composed containers
 
+#create traefik network
+docker network create --subnet=10.10.10.0/24 traefik_net
+
 # Start Traefik container
-# The Router should NAT forward 80 and 443 port to 40080 and 40443 of the traefik machine
-docker run -d --name reverse_proxy -p 48080:8080 -p 40080:80 -p 40443:443 -v $PWD/traefik.yaml:/etc/traefik/traefik.yaml -v $PWD/dynamic_config.yaml:/etc/traefik/dynamic_config.yaml traefik:latest
-
-# Start MariaDB container for nextcloud
-docker run -d --name mariadb-nc -p 43306:3306  -v <persistent-db-storage>:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=mariarootpwd -e MARIADB_DATABASE=nextclouddb -e MARIADB_USER=ncadmin -e MARIADB_PASSWORD=ncadminpwd mariadb:latest
-# Start Nextcloud container
-docker run -d --name nextcloud -p 30080:80 -v <persistent-data-storage>:/var/www/html nextcloud:latest
-
+docker run -d --name reverse_proxy -p 48080:8080 -p 40080:80 -p 40443:443 -v /share/Public/Container-data/traefik:/etc/traefik -v /share/Public/Container-data/traefik/letsencrypt:/letsencrypt traefik:latest
+docker network connect --ip 10.10.10.2 traefik_net reverse_proxy 
+# Remember to config NAT forwarding on your router to forward 80 and 443 ports to this container.
 
 # Start Ghost container
-docker run -d --name ghost_blog -p 2368:2368 -v <persistent-storage>:/var/lib/ghost/content ghost:latest 
+docker run -d --name ghost_blog --net traefik_net --ip 10.10.10.5 -v /share/Public/Container-data/ghost-blog:/var/lib/ghost/content ghost:latest 
+
+# Start MariaDB container
+docker run -d --name mariadb-nc --net traefik_net --ip 10.10.10.6  -v /share/Public/Container-data/nextcloud/mariadb:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=mariarootpwd -e MARIADB_DATABASE=nextclouddb -e MARIADB_USER=ncadmin -e MARIADB_PASSWORD=ncadminpwd mariadb:latest
+
+# Start Nextcloud container
+docker run -d --name nextcloud --net traefik_net --ip 10.10.10.7 -v /share/Public/Container-data/nextcloud/html-data:/var/www/html nextcloud:latest
+# in the first time access Nextcloud, input mariaDB information.
+
 # This container update Cloudflare DDNS 
 docker-compose -f cloud-ddns-compose.yaml up -d
+docker run -d --name cloudflare-ddns -v /share/Public/Container-data/cloudflare-ddns/config.json:/config.json -e PUID=1000 -e PGID=1000 timothyjmiller/cloudflare-ddns:latest
